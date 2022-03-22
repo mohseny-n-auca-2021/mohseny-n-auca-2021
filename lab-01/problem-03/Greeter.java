ea0d263e189779b88a5df8c0647b2499c5528d0b
mohseny_n <mohseny_n@auca.kg>
2022-03-22 16:34:39 +0100

    lab-01/problem-03 completed

--- lab-01/problem-03/Greeter.java
+++ lab-01/problem-03/Greeter.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class Greeter {
-	public static void main(String[] args ) {
+	public static void main(String[] args) {
 	Scanner scanner = new Scanner(System.in);
 	System.out.println("What is your full name?");
 	String fullName = scanner.nextLine();
